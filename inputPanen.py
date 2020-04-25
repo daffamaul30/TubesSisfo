ed8d2c211f38ace1c16c6c7cd2ff51dba1ef8a5c
Daffa <daffamaulana30@gmail.com>
2020-04-25 10:20:19 +0800

    Proses

---
+++ inputPanen.py
@@ -0,0 +1,37 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivy.uix.screenmanager import ScreenManager
+from kivy.factory import Factory
+from kivy.core.window import Window
+from kivymd.uix.picker import MDDatePicker
+
+Window.size = (350, 550)
+
+
+Builder.load_file("panen.kv")
+
+class Panen(MDApp):
+    def __init__(self, **kwargs):
+        self.title = "Hasil Panen"
+        self.theme_cls.theme_style = "Light"
+        self.theme_cls.primary_palette = "Blue"
+        self.sm = ScreenManager()
+        super().__init__(**kwargs)
+    
+    def build(self):
+        self.root = Factory.InputPanen()
+        # self.sm.add_widget()
+        # return self.sm
+
+    def show_date_picker(self, *args):
+            # print('AAA')
+            MDDatePicker(self.set_date).open()
+
+    def set_date(self, date_obj):
+        self.previous_date = date_obj
+        print(self.previous_date)
+        print(self.root.ids)
+        self.root.ids.date_picker_label.text = str(date_obj)
+    
+if __name__ == "__main__":
+    Panen().run()
