6b444df8992c8cecc09c03e54e18034910b2bca8
Daffa <daffamaulana30@gmail.com>
2020-04-25 14:58:54 +0800

    Progress

--- inputPanen.py
+++ inputPanen.py
@@ -5,6 +5,10 @@ from kivy.factory import Factory
 from kivy.core.window import Window
 from kivymd.uix.picker import MDDatePicker
 from kivymd.uix.picker import MDTimePicker
+from kivy.uix.boxlayout import BoxLayout
+from kivymd.uix.list import OneLineIconListItem, MDList
+from kivy.properties import StringProperty
+from kivymd.theming import ThemableBehavior
 
 Window.size = (350, 550)
 
@@ -23,7 +27,19 @@ class Panen(MDApp):
         self.root = Factory.InputPanen()
         # self.sm.add_widget()
         # return self.sm
-
+        
+    def on_start(self):
+        icons_item = {
+            "folder": "My files",
+            "account-multiple": "Shared with me",
+            "star": "Starred",
+            "history": "Recent",
+            "checkbox-marked": "Shared with me",
+            "upload": "Upload",
+        }
+        for icon_name in icons_item.keys():
+            self.root.ids.content_drawer.ids.md_list.add_widget(ItemDrawer(icon=icon_name, text=icons_item[icon_name]))
+    
     def show_date_picker(self, *args):
             # print('AAA')
             MDDatePicker(self.set_date).open()
@@ -41,6 +57,43 @@ class Panen(MDApp):
 
     def get_time(self, instance, time):
         self.root.ids.time_picker_label.text = str(time)
+    
+class ContentNavigationDrawer(BoxLayout):
+    pass
+
+
+class ItemDrawer(OneLineIconListItem):
+    icon = StringProperty()
+
+
+class DrawerList(ThemableBehavior, MDList):
+    def set_color_item(self, instance_item):
+        """Called when tap on a menu item."""
+
+        # Set the color of the icon and text for the menu item.
+        for item in self.children:
+            if item.text_color == self.theme_cls.primary_color:
+                item.text_color = self.theme_cls.text_color
+                break
+        instance_item.text_color = self.theme_cls.primary_color
+
+
+# class TestNavigationDrawer(MDApp):
+#     def build(self):
+#         return Builder.load_string(KV)
+
+#     def on_start(self):
+#         icons_item = {
+#             "folder": "My files",
+#             "account-multiple": "Shared with me",
+#             "star": "Starred",
+#             "history": "Recent",
+#             "checkbox-marked": "Shared with me",
+#             "upload": "Upload",
+#         }
+#         for icon_name in icons_item.keys():
+#             self.root.ids.content_drawer.ids.md_list.add_widget(
+#                 ItemDrawer(icon=icon_name, text=icons_item[icon_name]))
     
 if __name__ == "__main__":
     Panen().run()
