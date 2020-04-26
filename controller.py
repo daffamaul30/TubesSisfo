2d3e4e527ff479630e7ff9e88eb41504d49f53c1
Daffa <daffamaulana30@gmail.com>
2020-04-26 15:33:06 +0800

    26Apr

--- controller.py
+++ controller.py
@@ -1,6 +1,6 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
-from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.factory import Factory
 from kivy.core.window import Window
 from kivymd.uix.picker import MDDatePicker
@@ -11,20 +11,16 @@ from kivy.properties import StringProperty
 from kivymd.theming import ThemableBehavior
 import connection
 
+# Window.size = (350, 550)
 
-
-Window.size = (350, 550)
-
-
-Builder.load_file("panen.kv")
+class InputPanen(Screen):
+    pass
 
 class ContentNavigationDrawer(BoxLayout):
     pass
 
-
 class ItemDrawer(OneLineIconListItem):
     icon = StringProperty()
-
 
 class DrawerList(ThemableBehavior, MDList):
     def set_color_item(self, instance_item):
@@ -37,8 +33,7 @@ class DrawerList(ThemableBehavior, MDList):
                 break
         instance_item.text_color = self.theme_cls.primary_color
 
-
-class App(MDApp):
+class Main(MDApp):
     def __init__(self, **kwargs):
         self.title = "Frinsa"
         self.theme_cls.theme_style = "Light"
@@ -47,8 +42,7 @@ class App(MDApp):
         super().__init__(**kwargs)
         
     def build(self):
-        self.root = Factory.UI1()
-        return self.root
+        return Builder.load_file("Coba.kv")
     
     def on_start(self):
         icons_item = {
@@ -70,7 +64,6 @@ class App(MDApp):
 
     def set_date(self, date_obj):
         self.previous_date = date_obj
-        # print(self.root.ids)
         self.root.ids.date_picker_label.text = str(date_obj)
     
     def show_time_picker(self):
@@ -80,10 +73,8 @@ class App(MDApp):
         time_dialog.open()
 
     def get_time(self, instance, time):
+        self.ids.time_picker_label.text = str(time)
         
-        self.root.ids.time_picker_label.text = str(time)
-        
-
     def panen(self):
         jenis = self.root.ids.jenis.text
         berat = self.root.ids.berat.text
@@ -94,7 +85,5 @@ class App(MDApp):
         except:
             print("ERROR : ",self.root.ids.jenis.text,self.root.ids.berat.text)
         
-    
-
-if __name__ == "__main__":    
-    App().run()
+if __name__ == "__main__":
+    Main().run()
