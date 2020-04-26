78b56d0b0596fe14bde9ee19937620b84971c8bd
danilmuis <daniltelkom@gmail.com>
2020-04-26 23:21:34 +0700

    New DB Server

--- connection.py
+++ connection.py
@@ -14,20 +14,20 @@ def koneksi():
         print(e)
 
     finally:
-        if conn is not None and conn.is_connected():
-            conn.close()
+        return conn
+        # if conn is not None and conn.is_connected():
+        #     conn.close()
             
-def inputPanen(jenis,berat,tanggal,waktu):
+def inputPanen(jenis,berat):
     conn = mysql.connector.connect(host='52.149.60.155',
                                        database='kopi',
-                                       user='blog',
-                                       password='3a95b6d39a6863dc0f53577aac11de16')
+                                       user='kopi2',
+                                       password='kopi2')
     mycursor = conn.cursor()
-    query = "INSERT INTO biji_kopi (jenis_kopi,berat,tanggal,waktu) VALUES (%s,%s,%s,%s)"
+    query = "INSERT INTO biji_kopi (jenis_kopi,berat,tanggal,waktu) VALUES (%s,%s)"
     val = (jenis,berat)
     mycursor.execute(query,val)
     conn.commit()
     if mycursor.rowcount == 1:
         print("Data Added")
     
-koneksi()
