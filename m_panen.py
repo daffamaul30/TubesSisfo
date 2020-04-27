59919d742b8440709fcd28d0dd7dd8b23ce8cc41
danilmuis <daniltelkom@gmail.com>
2020-04-28 00:11:11 +0700

    m_panen delete data

--- m_panen.py
+++ m_panen.py
@@ -15,9 +15,19 @@ def inputPanen(jenis,berat,waktu,tanggal):
 def getPanen():
     conn = connection.koneksi()
     mycursor = conn.cursor()
-    #query = "SELECT * FROM biji_kopi"
-    query = "SELECT DATE_FORMAT(waktu,'%d/%m/%Y') as 'tanggal' FROM biji_kopi HAVING tanggal BETWEEN '25/04/2020' AND '30/04/2020' "
+    query = "SELECT * FROM biji_kopi"
+    #query = "SELECT DATE_FORMAT(waktu,'%d/%m/%Y') as 'tanggal' FROM biji_kopi HAVING tanggal BETWEEN '25/04/2020' AND '30/04/2020' "
     mycursor.execute(query)
     result = mycursor.fetchall()
     for x in result:
         print(x)
+    return result
+
+def deletePanen(id):
+    conn = connection.koneksi()
+    mycursor = conn.cursor()
+    query = "DELETE FROM biji_kopi WHERE id_panen = %i" %(id)
+    print(query)
+    mycursor.execute(query)
+    conn.commit()
+    print("Data Deleted")
