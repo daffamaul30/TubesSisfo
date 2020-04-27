f3ed3ae0082d6ca73b4562a46006760468cd55e4
danilmuis <daniltelkom@gmail.com>
2020-04-27 22:00:29 +0700

    split model

--- connection.py
+++ connection.py
@@ -18,22 +18,4 @@ def koneksi():
         # if conn is not None and conn.is_connected():
         #     conn.close()
             
-def inputPanen(jenis,berat,waktu,tanggal):
-    conn = koneksi()
-    mycursor = conn.cursor()
-    query = "INSERT INTO biji_kopi (jenis_kopi,berat,waktu,tanggal) VALUES (%s,%s,%s,%s)"
-    val = (jenis,berat,waktu,tanggal)
-    mycursor.execute(query,val)
-    conn.commit()
-    if mycursor.rowcount == 1:
-        print("Data Added")
-    
-def getPanen():
-    conn = koneksi()
-    mycursor = conn.cursor()
-    #query = "SELECT * FROM biji_kopi"
-    query = "SELECT DATE_FORMAT(waktu,'%d/%m/%Y') as 'tanggal' FROM biji_kopi HAVING tanggal BETWEEN '25/04/2020' AND '30/04/2020' "
-    mycursor.execute(query)
-    result = mycursor.fetchall()
-    for x in result:
-        print(x)
+
